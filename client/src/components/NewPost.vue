48a1198cc0d921c7eb1d5f4be54b09ab861bc4d7
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 17:29:38 +0800

    feat: create new post form

---
+++ client/src/components/NewPost.vue
@@ -0,0 +1,71 @@
+<script setup>
+import axios from 'axios'
+
+import { ref } from 'vue'
+
+const author = ref()
+const content = ref()
+
+function getTimestamp() {
+  const now = new Date();
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  };
+
+  return now.toLocaleString('en-US', options);
+}
+
+async function createPost() {
+    const timestamp = getTimestamp()
+
+    const response = await axios.post('http://localhost:6900/newpost', {
+        author: author.value,
+        content: content.value,
+        timestamp: timestamp
+    })
+    console.log(response)
+
+    author.value, content.value = null
+}
+</script>
+
+<template>
+    <form @submit.prevent="createPost" class="newpost-form">
+        <label class="input-label">Name</label>
+        <input class="author" v-model="author">
+        <label class="input-label">Content</label>
+        <input class="content" v-model="content">
+        <button class="submit-btn" type="submit">Create post</button>
+    </form>
+</template>
+
+<style scoped>
+form {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    padding: 20px;
+}
+
+.input-label {
+    margin-bottom: 5px;
+}
+
+input {
+    padding: 10px;
+    margin-bottom: 20px;
+    font-size: 16px;
+}
+
+.submit-btn {
+    padding: 10px 0px;
+}
+</style>
--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,9 +1,17 @@
 <script setup>
 import Header from '../components/Header.vue'
+import NewPost from '../components/NewPost.vue'
 </script>
 
 <template>
     <Header />
+    <main>
+        <NewPost />
+    </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+    padding: 50px;
+}
+</style>
