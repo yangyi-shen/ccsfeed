b24f9359ac5b1caf7588d36305e63093d9c7d9f7
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 17:22:36 +0800

    feat: add cors and request parsing to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const sqlite3 = require('sqlite3')
 
 const app = express()
+const PORT = 6900
+
+// allow http requests from any source
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+});
+
+// parse data from api requests into json form
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the backend for cssfeed!')
@@ -45,4 +57,9 @@ app.post('/newpost', (req, res) => {
     })
 })
 
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`)
+})
+
+// close database
 db.close()
