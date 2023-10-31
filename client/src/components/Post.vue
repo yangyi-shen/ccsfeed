9293565a83d78ac46d3a3960192f7499c85c1b53
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 17:39:08 +0800

    feat: create post component

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+defineProps({
+    author: String,
+    content: String,
+    timestamp: String
+})
+</script>
 
-<template></template>
+<template>
+    <div class="post">
+        <p class="author">{{ author }}</p>
+        <p class="timestamp">{{ timestamp }}</p>
+        <p class="content">{{ content }}</p>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.post {
+    background: rgb(228, 228, 228);
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px 0px;
+}
+
+.author {
+    font-weight: 600;
+    margin: 0px;
+}
+
+.timestamp {
+    color: grey;
+    margin: 0px;
+    margin-top: 5px;
+}
+</style>
