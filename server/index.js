02e4cc93711b52f9d851a585d72b6d342001e447
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 17:05:25 +0800

    feat: create /newpost api

--- server/index.js
+++ server/index.js
@@ -6,3 +6,24 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to the backend for cssfeed!')
 })
+
+app.post('/newpost', (req, res) => {
+    // make sure database is open
+    let db = new sqlite3.Database('./ccsfeed.db')
+
+    const author = req.body.author
+    const content = req.body.content
+    const timestamp = req.body.timestamp
+
+    db.run('INSERT INTO posts(author, content, timestamp) VALUES(?, ?, ?)', [author, content, timestamp], (err) => {
+        if (err) {
+            console.error(err)
+        }
+    })
+
+    res.json({
+        success: true
+    })
+})
+
+db.close()
