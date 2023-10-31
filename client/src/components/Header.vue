b7f1c6ba5770596f40db31dc70f5a7e708424488
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 16:54:00 +0800

    feat: created header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,5 +1,23 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <header>
+        <h1 class="logo">css<span class="blue-text">feed</span></h1>
+    </header>
+</template>
 
-<style scoped></style>
+<style scoped>
+header {
+    box-shadow: 0px 5px 5px rgb(219, 218, 218);
+    display: flex;
+    padding: 20px 50px;
+}
+
+.logo {
+    margin: 0px;
+}
+
+.blue-text {
+    color: #1e73be;
+}
+</style>
--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import Header from '../components/Header.vue'
+</script>
 
-<template></template>
+<template>
+    <Header />
+</template>
 
 <style scoped></style>
