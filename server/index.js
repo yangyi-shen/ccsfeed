4719497890a1207b4120cd8a6dba47af5d57a235
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 17:08:33 +0800

    feat: create /feed api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send('Welcome to the backend for cssfeed!')
 })
 
+app.get('/feed', (req, res) => {
+    db.all('SELECT * FROM posts', [], (err, rows) => {
+        if (err) {
+            console.error(err)
+            res.json({
+                success: false
+            })
+        } else {
+            res.json({
+                success: true,
+                feed: rows
+            })
+        }
+    })
+})
+
 app.post('/newpost', (req, res) => {
     // make sure database is open
     let db = new sqlite3.Database('./ccsfeed.db')
@@ -18,11 +34,14 @@ app.post('/newpost', (req, res) => {
     db.run('INSERT INTO posts(author, content, timestamp) VALUES(?, ?, ?)', [author, content, timestamp], (err) => {
         if (err) {
             console.error(err)
+            res.json({
+                success: false
+            })
+        } else {
+            res.json({
+                success: true
+            })
         }
-    })
-
-    res.json({
-        success: true
     })
 })
 
