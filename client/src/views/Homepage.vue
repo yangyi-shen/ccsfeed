77b803bd5e391b9c017c0281d25765d037710d6a
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-31 18:13:07 +0800

    feat: add newsfeed to homepage

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,12 +1,27 @@
 <script setup>
 import Header from '../components/Header.vue'
 import NewPost from '../components/NewPost.vue'
+import Post from '../components/Post.vue'
+
+import axios from 'axios'
+import { ref } from 'vue'
+
+const feed = ref()
+
+async function getFeed() {
+    const response = await axios.get('http://localhost:6900/feed')
+
+    feed.value = response.data.feed.reverse()
+}
+
+getFeed()
 </script>
 
 <template>
     <Header />
     <main>
-        <NewPost />
+        <NewPost :newpost="getFeed()" />
+        <Post v-for="item in feed" :author="item.author" :content="item.content" :timestamp="item.timestamp" />
     </main>
 </template>
 
