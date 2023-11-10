6a1a6240ed44caf75d4c7d7441c05973324db6ae
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-10 17:13:20 +0800

    feat: add routing for register page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/Homepage.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
     }
   ]
 })
