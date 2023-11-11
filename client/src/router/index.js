9e576ffa45037cb63016a485d709458f8a0ffe23
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-11 15:15:19 +0800

    feat: add routing for login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       component: () => import('../views/Homepage.vue')
     },
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
       path: '/register',
       name: 'register',
       component: () => import('../views/Register.vue')
