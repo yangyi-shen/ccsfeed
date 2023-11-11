64a1368a553bec919292970ce1c82bdbd7c45796
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-11 14:42:04 +0800

    feat: created register page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,56 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const username = ref()
+const password = ref()
+const router = useRouter()
+const message = ref(false)
+const error = ref(false)
+
+async function register() {
+    const response = await axios.post('http://localhost:6900/register', {
+        username: username.value,
+        password: password.value
+    })
+
+    const success = response.data.success
+    if (success) {
+        localStorage.setItem('signedin', true)
+        router.push('/')
+    } else {
+        error.value = !success
+    }
+}
+</script>
+
+<template>
+    <form class="form" @submit.prevent="register">
+        <label class="username-label">Username</label>
+        <input class="username" v-model="username">
+        <label class="password-label">Password</label>
+        <input class="password" v-model="password">
+        <button class="submit-btn" type="submit">Register</button>
+        <p class="error" v-if="error">Username already in use</p>
+    </form>
+</template>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.username {
+    margin-bottom: 10px;
+}
+
+.submit-btn {
+    margin-top: 10px;
+}
+
+.error {
+    color: red;
+}
+</style>
