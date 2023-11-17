d43a6b5b45cd93c943c4296b66adaf7ef24ff885
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-17 17:11:43 +0800

    chore: installed multer

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const multer = require('multer')
 
 // config environment variables
 require('dotenv').config();
