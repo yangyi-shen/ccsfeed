e796d486ec5f1f8e9a1ee817d020c0f8780ba3af
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-19 17:19:41 +0800

    feat: remove uauth question from sidebar faq

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -6,8 +6,6 @@
             <h2 class="faq-title">FAQ</h2>
             <p class="question">Q: Why does this site look so ugly?</p>
             <p class="answer">A: Web development takes a long time and I only had 4 hours, alright?</p>
-            <p class="question">Q: Why do I have to manually enter my name? This is worse than that wordpress thing!</p>
-            <p class="answer">A: Account login/registration is coming. Also you'll notice the feed is sorted chronologically, so it's way more convenient than wordpress.</p>
             <p class="question">Q: Omigosh Yang you're so awesome for making this on such short notice.</p>
             <p class="answer">A: I know, right? If we ever meet you can buy me a coffee.</p>
         </div>
