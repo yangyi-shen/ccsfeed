1a8f00859f4c3de8f1fdd710f4e5366bdba52555
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-11-20 17:43:04 +0800

    feat: add explanation for server outage

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -18,6 +18,7 @@ getFeed()
 <template>
     <div class="content">
         <NewPost :newpost="getFeed()" />
+        <p>Hey folks! Something's gone wrong big time with the server, so the posts won't be able to load. I'm gonna have it fixed by tommorow-until then, I am afraid that the feed will be unusable until then. Sorry sophie, I know how this is the only way you can chat with people. I'll try to fix the issue ASAP.</p>
         <Post v-for="item in feed" :author="item.author" :content="item.content" :timestamp="item.timestamp" :image="item.image" />
     </div>
 </template>
